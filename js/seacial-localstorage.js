7a8f2651443a9ea0947ff4952a88c73061fad696
Ayal Pinkus <apinkus@xs4all.nl>
2018-01-15 20:47:43 +0100

    Adding setting and getting username cookie.

--- js/seacial-localstorage.js
+++ js/seacial-localstorage.js
@@ -10,6 +10,7 @@ function validateLogin(username) {
         alert("User "+username+" does not exist. Please sign up first.");
         return false;
     }
+    setCookie("username",username);
     return true;
 } 
 
@@ -127,6 +128,30 @@ function getStorage()
 //    alert("storage is "+JSON.stringify(someLocalStorage));
 }
 
+
+
+function setCookie(name,value) {
+    var expires = "";
+    document.cookie = name + "=" + value + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return "";
+}
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+
+var request = new Array();
+var loggedInUsername = "";
+
 var prev_onload_localstorage = window.onload;
 window.onload = onload_localstorage;
 function onload_localstorage()
@@ -134,6 +159,19 @@ function onload_localstorage()
     if (prev_onload_localstorage) {
 	prev_onload_localstorage();
     }
+    
+    if (document.location.search) {
+	var i;
+	var vals=document.location.search.substr(1).split("&");
+	for (i in vals) {
+	    vals[i] = vals[i].replace(/\+/g, " ").split("=");
+	    request[unescape(vals[i][0])] = unescape(vals[i][1]);
+	}
+    }
+    
+    loggedInUsername = getCookie("username");
+alert("loggedInUsername = "+loggedInUsername);
+
     getStorage();
 }
 
