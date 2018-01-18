36f79f39a51653efdf64d2618eb210aa2a8fe833
Twan-Meijer <32161824+Twan-Meijer@users.noreply.github.com>
2018-01-18 18:05:01 +0100

    Requests html pages + chatbox html pages + scss seach bar

--- js/router.js
+++ js/router.js
@@ -20,6 +20,31 @@ switch (action) {
     case "dashboard-sailor":
         view("dashboard/_dashboard-sailor.html");
         break;
+
+    case "chatbox-sailor":
+        view("chatbox/_chatbox-sailor.html");
+        break;
+
+    case "chatbox-academic":
+        view("chatbox/_chatbox-academic.html");
+        break;
+
+    case "accepted-requests-sailor":
+        view("requests/_accepted-requests-sailor.html");
+        break;
+
+    case "my-requests-academic":
+        view("requests/_my-requests-academic.html");
+        break;
+
+    case "all-requests-sailor":
+        view("requests/_all-requests-sailor.html");
+        break;
+
+    case "all-requests-academic":
+        view("requests/_all-requests-academic.html");
+        break;
+
     default:
         view("error/_home.html");
         break;
